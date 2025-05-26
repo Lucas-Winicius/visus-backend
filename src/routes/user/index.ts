92d86fd8d30f206440104f4efb13a0f50c389f94
Lucas Winicius <lucasclucas7@gmail.com>
2025-05-26 20:11:17 -0300

    :sparkles: feat: Create user

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,8 +1,10 @@
 import { app } from '../../server'
 import get from './get'
-import post from './post'
+import createUser from './createUser'
+import login from './login'
 
 export default async function UserRoutes() {
-  app.register(post)
+  app.register(createUser)
+  app.register(login)
   app.register(get)
 }
